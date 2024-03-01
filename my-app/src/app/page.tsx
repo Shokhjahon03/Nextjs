d0764f26d178060d787fb14fe8a3b1dd58a04e9d
Shohjahon03 <quvvatovshohjahon707@gmail.com>
2024-03-02 04:28:53 +0500

    added

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import moon<File> from '../assets/Path.png'
+import moon from '../assets/Path.png'
 const HomePAge = () => {
   return (
     <>
@@ -8,7 +8,7 @@ const HomePAge = () => {
           <div className="navbar w-full h-[80px] flex justify-between items-center text-white">
               <h1 className='text-[24px] font-extrabold'>Where in the world?</h1>
               <button className='flex justify-between'>
-                <img src={moon} alt="alt" />
+                <img src={} alt="alt" />
                 <p>Darh Mode</p>
               </button>
           </div>
