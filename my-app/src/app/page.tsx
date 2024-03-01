907663040912a4e417c584a4085356929e5af994
Shohjahon03 <quvvatovshohjahon707@gmail.com>
2024-03-02 04:21:07 +0500

    added

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import moon<File> from '../assets/Path.png'
+const HomePAge = () => {
+  return (
+    <>
+     <nav className='bg-[#2B3844]'>
+        <div className="container">
+          <div className="navbar w-full h-[80px] flex justify-between items-center text-white">
+              <h1 className='text-[24px] font-extrabold'>Where in the world?</h1>
+              <button className='flex justify-between'>
+                <img src={moon} alt="alt" />
+                <p>Darh Mode</p>
+              </button>
+          </div>
+        </div>
+      </nav> 
+    </>
+  )
+}
+
+export default HomePAge
